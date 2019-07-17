53ea7c019636ebd951c54c889a06b6284d9f9fb9
Daniel-Choiniere <dpchoiniere@gmail.com>
2019-07-17 00:19:29 +0000

    updated function to not need a new array variable

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -14,16 +14,18 @@
 
 // sort the array and then loop through it and then see if the next index comapred to the previous index is the same as the current
 
-function containsDuplicates(a) {
-  var sorted = a.sort();
 
-  for (var i = 0; i < sorted.length; i++) {
-      if (sorted[i + 1] == sorted[i]) {
+function containsDuplicates(a) {
+  a.sort();
+
+  for (var i = 0; i < a.length; i++) {
+      if (a[i + 1] == a[i]) {
           return true;
       }
   }
   return false;
 }
+
 
 // VVVV trying to use the built in find function - not working VVVV
 
